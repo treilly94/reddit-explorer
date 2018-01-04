1921a1e936665b3cb7ea516be50917f843a245a7
treilly94 <treilly94@live.com>
2018-01-04 16:46:15 +0000

    Made it so that it can add posts made before or after a existing dataset

--- data_collector.py
+++ data_collector.py
@@ -2,6 +2,7 @@ import requests
 import requests.auth
 import json
 import pandas as pd
+import os.path
 
 
 def credentials_reader():
@@ -17,21 +18,48 @@ def get_auth(creds):
     return response.json()
 
 
-def get_saved_json(creds, auth):
+def get_json(creds, auth, before=None, after=None):
+
     headers = {"Authorization": "bearer %s" % auth.get("access_token"),
                "User-Agent": "Reddit_explorer by %s" % creds.get("username")}
-    response = requests.get("https://oauth.reddit.com/user/%s/saved" % creds.get("username"), headers=headers)
+
+    url = "https://oauth.reddit.com/user/%s/saved?limit=100" % creds.get("username")
+
+    if before:
+        url += "&before=" + before
+
+    if after:
+        url += "&after=" + after
+
+    response = requests.get(url, headers=headers)
     return response.json()
 
 
 def post_extractor(raw):
-    children = raw.get("data").get("children")
-    children = [i.get("data") for i in children]
+    try:
+        children = raw.get("data").get("children")
 
-    columns = ["created", "author", "num_comments", "over_18", "permalink", "score", "subreddit", "title"]
-    df = pd.DataFrame(children)[columns]
+        # Combine the post kind with the post data
+        vals = []
+        for i in children:
+            a = i.get("data")
+            a["kind"] = i.get("kind")
+            vals.append(a)
 
-    return df
+        columns = ["kind", "id", "created", "author", "num_comments", "over_18", "permalink", "score", "subreddit", "title"]
+        return pd.DataFrame(vals)[columns]
+
+    except:
+        return pd.DataFrame(columns=columns)
+
+
+def get_posts(creds, auth, before=None, after=None):
+    # Get raw data
+    raw = get_json(creds, auth, before, after)
+    # Process data
+    posts = post_extractor(raw)
+
+    return posts
 
 
 def data_collector():
@@ -43,15 +71,34 @@ def data_collector():
     auth = get_auth(creds)
     print(auth)
 
-    # Get raw saved
-    raw = get_saved_json(creds, auth)
-    print(raw)
+    # Check to see if there is already data
+    if os.path.exists("./saved_posts.csv"):
+        # Get existing dataset
+        print("Getting existing dataset")
+        data = pd.read_csv("./saved_posts.csv", header=0)
 
-    # Get posts
-    posts = post_extractor(raw)
+        # Get the id of the top and bottom posts in the dataset
+        head = data.head(1)
+        head_id = head["kind"].iloc[0] + "_" + head["id"].iloc[0]
+        print("Head id: %s" % head_id)
+        tail = data.tail(1)
+        tail_id = tail["kind"].iloc[0] + "_" + tail["id"].iloc[0]
+        print("Tail id: %s" % tail_id)
+
+        # Get the posts from before and after the current dataset
+        before = get_posts(creds, auth, before=head_id)
+        after = get_posts(creds, auth, after=tail_id)
+
+        # Concatenate back into one dataframe
+        data = data.append(after)
+        data = before.append(data)
+
+    else:
+        data = get_posts(creds, auth)
+
 
     # Write posts to file
-    posts.to_csv("saved_posts.csv", index=False)
+    data.to_csv("saved_posts.csv", index=False)
 
 
 if __name__ == "__main__":
